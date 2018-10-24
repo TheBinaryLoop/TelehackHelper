aec3f0d233c39c7915fe8072c8d4678be5f212ad
Lukas Eßmann <l.essmann@directonline.de>
2018-10-24 12:48:12 +0200

    Updated Console Programm

--- TelehackHelper.Console/Program.cs
+++ TelehackHelper.Console/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace TelehackHelper.Console
+﻿using System;
+using TelehackHelper.Core;
+using CL = System.Console;
+
+namespace TelehackHelper.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CL.Write("Creating database...");
+            Database db = new Database();
+            CL.WriteLine("Done");
 
+            CL.ReadLine();
         }
     }
 }
