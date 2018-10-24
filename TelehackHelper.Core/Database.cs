9c106769c26fe36caf6402ae10034950a2657dc2
Lukas Eßmann <l.essmann@directonline.de>
2018-10-24 12:47:47 +0200

    Added AreaCode class with default values Updated BBS class Added Database class

---
+++ TelehackHelper.Core/Database.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using SQLite;
+using TelehackHelper.Core.Entities;
+
+namespace TelehackHelper.Core
+{
+    public class Database
+    {
+        private SQLiteConnection _db;
+
+        public SQLiteConnection Connection
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    _db = new SQLiteConnection("TelehackHelper.db");
+                }
+                return _db;
+            }
+        }
+
+        public Database()
+        {
+            Connection.RunInTransaction(() => {
+                Connection.Execute("CREATE TABLE IF NOT EXISTS `AreaCode` ( `Id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `State` varchar NOT NULL, `Code` integer NOT NULL UNIQUE );");
+                Connection.Execute("CREATE TABLE IF NOT EXISTS `BBS` ( `Id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `Name` varchar, `AreaCodeId` integer NOT NULL, `Number` varchar ( 9 ) NOT NULL, `SysOP`	integer NOT NULL DEFAULT 0, CONSTRAINT `AreaCodeId` FOREIGN KEY(`Id`) REFERENCES `AreaCode` );");
+            });
+            Connection.RunInTransaction(() =>
+            {
+                AreaCode.AreaCodes.Select(ac => $"INSERT OR IGNORE INTO `AreaCode`(`State`, `Code`) VALUES ('{ac.State}', {ac.Code});").ToList().ForEach((string statement) => {
+                    Connection.Execute(statement);
+                });
+            });
+        }
+    }
+}
--- TelehackHelper.Core/Entities/BBS.cs
+++ TelehackHelper.Core/Entities/BBS.cs
@@ -1,6 +1,16 @@
-﻿namespace TelehackHelper.Core.Entities
+﻿using SQLite;
+
+namespace TelehackHelper.Core.Entities
 {
     public class BBS
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [NotNull]
+        public int AreaCodeId { get; set; }
+        [Unique, MaxLength(9), NotNull]
+        public string Number { get; set; }
+        public bool SysOP { get; set; } = false;
     }
 }
